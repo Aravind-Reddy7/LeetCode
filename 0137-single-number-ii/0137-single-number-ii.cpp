bcea4339681917319a37a400de62916a9a28bd79
Aravind-Reddy7 <aravindreddymadireddy07@gmail.com>
2025-02-19 19:22:23 +0530

    Time: 1 ms (44.84%), Space: 13.3 MB (65.54%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for(int i = 0; i < n - 2; ) {
+            if(nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) 
+                i += 3;
+            else
+                return nums[i];
+        }
+
+        return nums[n - 1];
+    }
+};
