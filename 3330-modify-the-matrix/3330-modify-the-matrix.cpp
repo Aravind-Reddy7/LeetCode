ac8507e7908a21aab0c00921e47262b544ddefd8
Aravind-Reddy7 <aravindreddymadireddy07@gmail.com>
2025-03-20 15:56:18 +0530

    Time: 4 ms (14.32%), Space: 23.6 MB (38.99%) - LeetHub

---
+++ 3330-modify-the-matrix/3330-modify-the-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
+
+        for(int i = 0; i < matrix.size(); i++) {
+            for(int j = 0; j < matrix[i].size(); j++) {
+                if(matrix[i][j] == -1) {
+                    
+                    int max = -1;
+
+                    for(int k = 0; k < matrix.size(); k++) 
+                        if(max < matrix[k][j])
+                            max = matrix[k][j];
+
+                    matrix[i][j] = max;
+                    
+                }
+            }
+        }
+
+        return matrix;
+    }
+};
