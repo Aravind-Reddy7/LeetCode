85bf222ecb69f6dc5fe0ab7fc92427f59ed7ece1
Aravind-Reddy7 <aravindreddymadireddy07@gmail.com>
2025-03-30 20:57:37 +0530

    Time: 1 ms (29.44%), Space: 11.7 MB (68.67%) - LeetHub

--- 0979-di-string-match/0979-di-string-match.cpp
+++ 0979-di-string-match/0979-di-string-match.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> diStringMatch(string s) {
         
-        int high = s.size();
+        int high = s.size();   // Inisialisation 
         int low = 0;
         vector<int> result;
 
         for(int i = 0; i < s.size(); i++) {
             if(s[i] == 'I')
-                result.push_back(low++);
+                result.push_back(low++); // if s[i] == 'I' push LOW value
             else
-                result.push_back(high--);
+                result.push_back(high--); // if s[i] == 'D' push High value
         }
 
-        result.push_back(low++);
+        result.push_back(low++);  
 
-        return result;
+        return result; // return RESULT
 
     }
 };
