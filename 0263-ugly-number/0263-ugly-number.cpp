918fc606e7780132b8b4ffd099d8cdae9ee2474f
Aravind-Reddy7 <aravindreddymadireddy07@gmail.com>
2025-06-06 10:34:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (78.71%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+
+        if(n <= 0)
+            return false;
+        
+        int factor[3] = {2, 3, 5};
+
+        for(int i = 0; i < 3; i++) {
+            while(n % factor[i] == 0) {
+                n = n / factor[i];
+            }
+        }
+
+        return n == 1;
+    }
+};
