b08ff7f5c4de6afae09bae5330abecad89136784
Aravind-Reddy7 <aravindreddymadireddy07@gmail.com>
2025-06-25 08:28:56 +0530

    Time: 0 ms (100%), Space: 18.4 MB (56.49%) - LeetHub

--- 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
+++ 2163-kth-distinct-string-in-an-array/2163-kth-distinct-string-in-an-array.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
-        
-        string result = "";
-        int n = arr.size();
+
+        unordered_map<string, int> freq;
+
+        // Count the frequencies
+
+        for(const string &word : arr) {
+                freq[word]++;
+        }
+
+        // Find the k-th frequency
+
         int count = 0;
 
-        for(int i = 0; i < n; i++) {
-            bool found = true;
-            for(int j = 0; j < n; j++) {
-                if(arr[i] == arr[j] && i != j) {
-                    found = false;
-                    break;
-                }
-            }
-
-            if(found == true) {
+        for(const string &word : arr) {
+            if (freq[word] == 1) {
                 count += 1;
+
                 if(count == k)
-                    return arr[i];
+                    return word;
             }
         }
 
-        return result;
+        return "";
+        
+        // string result = "";
+        // int n = arr.size();
+        // int count = 0;
+
+        // for(int i = 0; i < n; i++) {
+        //     bool found = true;
+        //     for(int j = 0; j < n; j++) {
+        //         if(arr[i] == arr[j] && i != j) {
+        //             found = false;
+        //             break;
+        //         }
+        //     }
+
+        //     if(found == true) {
+        //         count += 1;
+        //         if(count == k)
+        //             return arr[i];
+        //     }
+        // }
+
+        // return result;
     }
 };
